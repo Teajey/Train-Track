caef7f38b00e3f8f5358b4add01084c4cb3ed29c
Thomas Williams <thomas.joel.williams@gmail.com>
2019-08-25 10:18:15 +1200

    Add API functions

--- script.js
+++ script.js
@@ -1,48 +1,28 @@
 $(() => {
-    // $.ajax({
-    //     url: "https://www.metlink.org.nz/api/v1/ServiceLocation/HVL",
-    //     method: "GET"
-    // }).done(servLocation => {
-    //     // $.ajax({
-    //     //     url: "https://www.metlink.org.nz/api/v1/StopDepartures/UPPE",
-    //     //     method: "GET"
-    //     // }).done(stopDepartures => {
 
-    //     // });
-    //     console.log(servLocation);
-    //     let txt = '';
-    //     data.Services.forEach(train => {
-    //         txt = txt.concat(`
+    stopDepartures("HVL", serviceMap => {
+        console.log(serviceMap);
+    });
+
+    // $("#queryForm").submit(function (e) {
+    //     e.preventDefault();
+    //     let servLocation = $(this).find("#serviceLocationOptions").val();
+    //     $.ajax({
+    //         url: `https://www.metlink.org.nz/api/v1/ServiceLocation/${servLocation}`,
+    //         method: "GET"
+    //     }).done((data) => {
+    //         let txt = '';
+    //         data.Services.forEach(train => {
+    //             txt = txt.concat(`
     //         VehicleRef: ${train.VehicleRef}<br>
     //         BehindSchedule: ${train.BehindSchedule}<br>
     //         DelaySeconds: ${train.DelaySeconds}<br>
-    //         RecordedAtTime: ${train.RecordedAtTime}<br>
+    //         RecordedAtTime: ${new Date(train.RecordedAtTime).toTimeString().substring(0, 8)}<br>
     //         Direction: ${train.Direction}<br>
+    //         HasStarted: ${train.HasStarted}<br>
     //         <br>`);
+    //         });
+    //         $("#result").html(txt);
     //     });
-    //     $("#result").html(txt);
     // });
-    console.log("Document ready");
-
-    $("#queryForm").submit(function (e) {
-        e.preventDefault();
-        let servLocation = $(this).find("#serviceLocationOptions").val();
-        $.ajax({
-            url: `https://www.metlink.org.nz/api/v1/ServiceLocation/${servLocation}`,
-            method: "GET"
-        }).done((data) => {
-            let txt = '';
-            data.Services.forEach(train => {
-                txt = txt.concat(`
-            VehicleRef: ${train.VehicleRef}<br>
-            BehindSchedule: ${train.BehindSchedule}<br>
-            DelaySeconds: ${train.DelaySeconds}<br>
-            RecordedAtTime: ${new Date(train.RecordedAtTime).toTimeString().substring(0, 8)}<br>
-            Direction: ${train.Direction}<br>
-            HasStarted: ${train.HasStarted}<br>
-            <br>`);
-            });
-            $("#result").html(txt);
-        });
-    });
 });
---
+++ util.js
@@ -0,0 +1,95 @@
+// Functions that interface with API
+
+
+/**
+ * Provides the train stops matching the given string. (Minimum of 3 characters).
+ * 
+ * @param {*} string Find a stop matching this string
+ * @param {*} cb Function in which the stop is returned
+ * 
+ * Ex: Prints ID, code, and name of Naenae Station.
+ * stopSearch("nae", stop => {
+ *   console.log(stop);
+ * });
+ * 
+ */
+function stopSearch(string, callback) {
+    $.ajax({
+        url: `https://www.metlink.org.nz/api/v1/StopSearch/${string}`,
+        method: "GET"
+    }).done(function (data) {
+        let trainStop = data[0];
+        console.log(trainStop);
+        callback(isNaN(trainStop.Sms) ? trainStop : null);
+    }).fail(function (err) {
+        callback(err.statusText);
+    });
+}
+
+/**
+ * Provides locations of vehicles on the given route
+ * 
+ * @param {*} routeCode 
+ * @param {*} callback Function in which the service locations are returned 
+ * 
+ * serviceLocation("HVL", serviceLocation => {
+ *     console.log(serviceLocation);
+ * });
+ * 
+ */
+function serviceLocation(routeCode, callback) {
+    $.ajax({
+        url: `https://www.metlink.org.nz/api/v1/ServiceLocation/${routeCode}`,
+        method: "GET"
+    }).done(function (serviceLocation) {
+        callback(serviceLocation);
+    }).fail(function (err) {
+        callback(err.statusText);
+    });
+}
+
+/**
+ * Provides a list of stops on the given route.
+ * 
+ * @param {*} routeCode 
+ * @param {*} callback Function in which the service stops are returned 
+ * 
+ * serviceMap("HVL", serviceMap => {
+ *    console.log(serviceMap);
+ * });
+ * 
+ */
+function serviceMap(routeCode, callback) {
+    $.ajax({
+        url: `https://www.metlink.org.nz/api/v1/ServiceMap/${routeCode}`,
+        method: "GET"
+    }).done(function (serviceMap) {
+        callback(serviceMap);
+    }).fail(function (err) {
+        callback(err.statusText);
+    });
+}
+
+/**
+ * Provides a list of stops on the given route.
+ * 
+ * @param {*} stopCode 
+ * @param {*} callback Function in which the stop departures are returned 
+ * 
+ * stopDepartures("WELL", serviceMap => {
+ *     console.log(serviceMap);
+ * });
+ * 
+ */
+function stopDepartures(stopCode, callback) {
+    $.ajax({
+        url: `https://www.metlink.org.nz/api/v1/StopDepartures/${stopCode}`,
+        method: "GET"
+    }).done(function (stopDepartures) {
+        console.log(stopDepartures);
+        callback(stopDepartures);
+    }).fail(function (err) {
+        callback(err.statusText);
+    });
+}
+
